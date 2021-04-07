e996b38cec2288da048e7c0c2d4d622b4fc3f8ba
Lev <l.misiliuk@splinestudio.ua>
2021-04-08 01:55:34 +0300

    Initilized swiper, added photos, changed some styles

---
+++ js/script.js
@@ -0,0 +1,12 @@
+new Swiper('.swiper-container', {
+    //Arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    //How many slides show
+    slidesPerView: 4,
+
+    autoHeight: true,
+});
