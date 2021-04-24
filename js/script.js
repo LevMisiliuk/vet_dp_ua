96b270a19bfc5fca11d2ec0d73eef30d6c509cd7
Lev <l.misiliuk@splinestudio.ua>
2021-04-24 16:17:39 +0300

    [marin slider] Reworked main slider

--- js/script.js
+++ js/script.js
@@ -19,20 +19,81 @@ $(function() {
     })
 
     
-    $('.slider-main-image').slick({
+    $('.slider-single').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: false,
+      adaptiveHeight: true,
+      infinite: false,
+     useTransform: true,
+      speed: 400,
+      cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+    });
+
+    $('.slider-text').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: false,
+      adaptiveHeight: true,
+      infinite: false,
+     useTransform: true,
+      speed: 400,
+      cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+    });
+   
+    $('.slider-nav')
+      .on('init', function(event, slick) {
+        $('.slider-nav .slick-slide.slick-current').addClass('is-active');
+      })
+      .slick({
         slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-navigation'
-      });
-      $('.slider-navigation').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: '.slider-main-image',
+        slidesToScroll: 4,
         dots: false,
-        centerMode: false,
-        focusOnSelect: true
+        focusOnSelect: false,
+        infinite: true,
+        responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+          }
+        }, {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+         }
+        }, {
+          breakpoint: 420,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+       }
+        }]
       });
+   
+    $('.slider-single').on('afterChange', function(event, slick, currentSlide) {
+      $('.slider-nav').slick('slickGoTo', currentSlide);
+      var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+      $('.slider-nav .slick-slide.is-active').removeClass('is-active');
+      $(currrentNavSlideElem).addClass('is-active');
+    });
+
+    $('.slider-text').on('afterChange', function(event, slick, currentSlide) {
+      $('.slider-nav').slick('slickGoTo', currentSlide);
+      var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+      $('.slider-nav .slick-slide.is-active').removeClass('is-active');
+      $(currrentNavSlideElem).addClass('is-active');
+    });
+   
+    $('.slider-nav').on('click', '.slick-slide', function(event) {
+      event.preventDefault();
+      var goToSingleSlide = $(this).data('slick-index');
+   
+      $('.slider-single').slick('slickGoTo', goToSingleSlide);
+      $('.slider-text').slick('slickGoTo', goToSingleSlide);
+    });
 
 })
